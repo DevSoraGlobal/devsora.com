298f4f743eb6516f0e088c857cccd31b3b01e3dc
HB Singh Choudhary <hb.singh.choudhary@gmail.com>
2025-07-18 20:13:08 +0000

    remove the blank area above hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <NavBar />
-      <main className="flex-grow pt-16">
+      <main className="flex-grow">
         <HeroSection />
         <IntroSection />
         <CoursesSection />
