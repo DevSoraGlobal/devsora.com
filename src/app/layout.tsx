9e97b7713e58b371c6b51230fc14d325ec4bba54
HB Singh Choudhary <hb.singh.choudhary@gmail.com>
2025-08-01 17:55:51 +0000

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: "Devsora",
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={cn("font-body antialiased", "min-h-screen bg-background font-sans")}>
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
